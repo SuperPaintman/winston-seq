3080728d82e1a2c6c0001e16be0a1686220b4ff4
Brunno Bettini <brunnobb@gmail.com>
2021-12-03 00:04:28 -0300

    add spreadProps to IOptions

--- src/index.ts
+++ src/index.ts
@@ -49,7 +49,7 @@ interface IOption {
   level?: string;
   silent?: boolean;
   handleExceptions?: boolean;
-
+  spreadProps:boolean;
   serverUrl?: string;
   apiKey?: string;
   maxBatchingTime?: number;
@@ -343,6 +343,20 @@ export class Transport extends TransportStream {
 
         const ts = Date.parse(timestamp);
 
+        let propertiesParam;
+        if (this.options.spreadProps){
+          propertiesParam =  {
+            applicationName: this.options.applicationName,
+          ...(properties as Object)}
+        }
+        else{
+          propertiesParam =  {
+            applicationName: this.options.applicationName,
+            data: properties
+          }
+
+        }
+
         const seqEvent: SeqEvent = {
           timestamp:
             timestamp && !Number.isNaN(ts)
@@ -350,10 +364,7 @@ export class Transport extends TransportStream {
               : new Date(),
           level: this.levelMapper(level),
           messageTemplate: message,
-          properties: {
-            applicationName: this.options.applicationName,
-            ...(properties as Object)
-          },
+          properties:propertiesParam,
           exception:
             errors.length > 0
               ? errors
